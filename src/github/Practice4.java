5fa0ab40229ac66a168c5566a21818d461544809
somnatha656 <somnatha656@gmail.com>
2022-09-13 15:04:30 +0100

    forth commit

---
+++ src/github/Practice4.java
@@ -0,0 +1,14 @@
+package github;
+
+public class Practice4 {
+    public static void main(String[] args) {
+        int x = 15;
+        int y = 10;
+        int z = x + y;
+        System.out.println(x);
+        System.out.println(y);
+        System.out.println(z);
+
+
+}
+}
