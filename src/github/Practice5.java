d64ac34ec3d7485f4803994c3fd570d232e5490f
somnatha656 <somnatha656@gmail.com>
2022-09-13 15:08:33 +0100

    fifth committ

---
+++ src/github/Practice5.java
@@ -0,0 +1,17 @@
+package github;
+
+public class Practice5 {
+    public static void main(String[] args) {
+
+        char a = 'J';
+        long b = 2254554585788L;
+        boolean c = false;
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+
+
+    }
+
+}
