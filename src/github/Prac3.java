50b36747b8b70867764d5ca67525050b22e36cd3
somnatha656 <somnatha656@gmail.com>
2022-09-13 14:56:14 +0100

    third commit

---
+++ src/github/Prac3.java
@@ -0,0 +1,12 @@
+package github;
+
+public class Prac3 {
+    public static void main(String[] args) {
+        String myName = "Jalpa";
+        String myAddress = "91, Grove road";
+
+        System.out.println(myName);
+        System.out.println(myAddress);
+
+    }
+}
